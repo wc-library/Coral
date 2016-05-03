bd299b4c960f41866d59404660c527525796ef51
Ian Walls <iwalls@umass.edu>
2016-05-03 15:03:59 -0400

    Exit after redirecting to login screen
    Once it has been determined that a user needs to login before proceeding, stop executing PHP
    code, in case the client does not respect Location headers by default (such as curl).

--- resources/user.php
+++ resources/user.php
@@ -52,7 +52,7 @@ if ($config->settings->authModule == 'Y'){
 
 		$authURL = $util->getCORALURL() . "auth/" . $addURL . htmlentities($_SERVER['REQUEST_URI']);
 		header('Location: ' . $authURL, true);
-
+		exit("redirecting to " . $authURL);
 	}
 
 
