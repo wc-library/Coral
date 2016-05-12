db5c9e1c798c2ce7d8a9b860867c0554293aecb7
Matthias Meusburger <matthias.meusburger@biblibre.com>
2016-05-12 10:32:19 +0200

    Issue#7: New Resource API
     - Start workflow when available

--- resources/admin/classes/domain/Resource.php
+++ resources/admin/classes/domain/Resource.php
@@ -2131,7 +2131,6 @@ class Resource extends DatabaseObject {
 
 			}
 
-
 			//Start the first step
 			//this handles updating the db and sending notifications for approval groups
 			foreach ($this->getFirstSteps() as $resourceStep) {
