ccd82e2730d992a4172582ca380e66e0f7218bc6
James Cuénod <j3frea@gmail.com>
2016-06-04 12:26:09 -0500

    Abstracting per module db test wrt pre-existing db

--- auth/install/auth.php
+++ auth/install/auth.php
@@ -34,6 +34,13 @@ function register_auth_requirement()
 
 			// TODO: abstract this code (cf. licensing, management)
 			//make sure the tables don't already exist - otherwise this script will overwrite all of the data!
+			$table_to_check = "User";
+			$module_name = "Authentication"; // %s ...
+			$non_empty_fail_message = _("The tables for %s already exist. If you intend to upgrade, please run upgrade.php instead. If you would like to perform a fresh install you will need to manually drop all of the tables in this schema first.");
+			$dbconn;
+			$dbname = $shared_module_info[$MODULE_VARS["uid"]]['db_name'];
+
+
 			if ($shared_module_info[$MODULE_VARS["uid"]]["db_feedback"] == 'already_existed')
 			{
 				try
@@ -44,7 +51,7 @@ function register_auth_requirement()
 					if ($result->numRows() > 0 )
 					{
 						$return->success = false;
-						$return->yield->messages[] = _("The Authentication tables already exist.  If you intend to upgrade, please run upgrade.php instead.  If you would like to perform a fresh install you will need to manually drop all of the Authentication tables in this schema first.");
+						$return->yield->messages[] = _("The tables for Authentication already exist. If you intend to upgrade, please run upgrade.php instead.  If you would like to perform a fresh install you will need to manually drop all of the Authentication tables in this schema first.");
 						require_once "install/templates/try_again_template.php";
 						$return->yield->body = try_again_template();
 						return $return;
@@ -52,6 +59,8 @@ function register_auth_requirement()
 				}
 				catch (Exception $e)
 				{
+					//TODO: this should be handled much better! if the table already existed we need to figure out more about it...
+					$return->yield->messages[] = var_export($e, 1);
 					//TODO: This may indicate a halfway done installation at some point
 					$return->success = false;
 					$return->yield->messages[] = _("Please verify your database user has access to select from the information_schema MySQL metadata database.");
