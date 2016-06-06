752f4616a7d2aa3b4c118a2c10ff21bcb3b316a7
Blake Eiten <baeiten@gmail.com>
2016-06-06 14:36:00 -0500

    More corrections of 1.X update

--- licensing/install/protected/update_NEXT.sql
+++ licensing/install/protected/update_NEXT.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `SFXProvider` CHANGE `SFXProvider` `sfxProvider` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT; 
-ALTER TABLE `expression` CHANGE `documentText` `documentText` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
+ALTER TABLE `Expression` CHANGE `documentText` `documentText` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
