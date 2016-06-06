49b990e9b3284b9bf2f891d21b9ad77a90c488a4
James Cuénod <j3frea@gmail.com>
2016-06-06 16:18:34 -0500

    Busy with organization module (end of day commit)

--- organizations/install/organizations.php
+++ organizations/install/organizations.php
@@ -9,7 +9,7 @@ function register_organizations_requirement()
 			return [
 				"database" => [
 					"title" => _("Organizations Database"),
-					"default_value" => "coral_organizations_prod"
+					"default_value" => "coral_organizations"
 				],
 				"config_file" => [
 					"path" => "auth/admin/configuration.ini",
@@ -31,6 +31,83 @@ function register_organizations_requirement()
 			$result = $shared_module_info["provided"]["check_db"]($dbconnection, $shared_module_info[$MODULE_VARS["uid"]], "Organization", $MODULE_VARS["translatable_title"]);
 			if ($result)
 				return $result;
+
+			// Process sql files
+			$sql_files_to_process = ["organizations/install/test_create.sql", "organizations/install/create_tables_data.sql"];
+			$ret = $shared_module_info["provided"]["process_sql_files"]( $dbconnection, $sql_files_to_process, $MODULE_VARS["uid"] );
+			if (!$ret["success"])
+			{
+				$return->success = false;
+				$return->yield->messages = array_merge($return->yield->messages, $ret["messages"]);
+				return $return;
+			}
+
+			$shared_module_info["provided"]["set_up_admin_in_db"]($dbconnection, $shared_module_info["common"]["default_user"]["username"]);
+
+			// Organization can use ldap
+			$authIni = parse_ini_file($shared_module_info["auth"]["config_file"]["path"]);
+
+			// TODO: Find out whether ldap here is the same as in auth
+			if ($authIni["ldap_enabled"])
+			{
+				$ldap_explanation = "The following are optional for LDAP if you wish to have user's first and last name automatically populated";
+				$ldap_fields = [
+					// [
+					// 	"key" => "ldap_host",
+					// 	"title" => _("Host"),
+					// 	"default_value" => isset($authIni["ldap"]["host"]) ? $authIni["ldap"]["host"] : ""
+					// ],[
+					// 	"key" => "ldap_port",
+					// 	"title" => _("Port"),
+					// 	"default_value" => isset($authIni["ldap"]["port"]) ? $authIni["ldap"]["port"] : ""
+					// ],[
+					// 	"key" => "ldap_search_key",
+					// 	"title" => _("Search Key"),
+					// 	"default_value" => isset($authIni["ldap"]["search_key"]) ? $authIni["ldap"]["search_key"] : ""
+					// ],[
+					// 	"key" => "ldap_base_dn",
+					// 	"title" => _("Base DN"),
+					// 	"default_value" => isset($authIni["ldap"]["base_dn"]) ? $authIni["ldap"]["base_dn"] : ""
+					// ],
+					[
+						"key" => "ldap_fname_field",
+						"title" => _("First Name"),
+						"default_value" => isset($authIni["ldap"]["fname"]) ? $authIni["ldap"]["fname"] : ""
+					],[
+						"key" => "ldap_lname_field",
+						"title" => _("Last Name"),
+						"default_value" => isset($authIni["ldap"]["lname"]) ? $authIni["ldap"]["lname"] : ""
+					]
+				];
+			}
+
+			// $iniData = array();
+			// $iniData["settings"] =[
+			// 	"licensingModule" => $licensingModule,
+			// 	"licensingDatabaseName" => $licensingDatabaseName,
+			// 	"authModule" => $authModule,
+			// 	"authDatabaseName" => $authDatabaseName,
+			// 	"usageModule" => $usageModule,
+			// 	"resourcesModule" => $resourcesModule,
+			// 	"resourcesDatabaseName" => $resourcesDatabaseName,
+			// 	"remoteAuthVariableName" => $remoteAuthVariableName
+			// ];
+			//
+			// $iniData["database"] = [
+			// 	"type" => "mysql",
+			// 	"host" => $database_host,
+			// 	"name" => $database_name,
+			// 	"username" => $database_username,
+			// 	"password" => $database_password
+			// ];
+			//
+			// $iniData["ldap"] = [
+			// 	"host" => $ldap_host,
+			// 	"search_key" => $search_key,
+			// 	"base_dn" => $base_dn,
+			// 	"fname_field" => $fname_field,
+			// 	"lname_field" => $lname_field
+			// ];
 
 			return $return;
 		}
