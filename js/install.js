59ccc760f74ed6b8927cba321cc03ad23fd658ac
James Cuénod <j3frea@gmail.com>
2016-06-09 10:40:31 -0500

    Form element selector to use `:input` not `input`

--- js/install.js
+++ js/install.js
@@ -148,7 +148,7 @@ $(document).ready(function(){
 	submit_install_step();
 }).on("submit", function(){
 	$(".main").animate({"opacity": 0, "paddingRight": 30 }, 200, function(){
-		var $form_elements = $("form input:visible");
+		var $form_elements = $("form :input:visible");
 		//But sometimes checkboxes are hidden and their labels are used:
 		$form_elements = $form_elements.add($(":checkbox:hidden").filter(function(){
 			return $("label[for='" + $(this).attr("id") + "']").is(":visible");
