16e39bc24b38df3526098dd0bcefd5569de9bc5c
James Cuénod <j3frea@gmail.com>
2016-06-16 18:06:34 -0500

    Working on default_db_user postInstallationTest

--- install/requirements/have_default_db_user.php
+++ install/requirements/have_default_db_user.php
@@ -72,42 +72,64 @@ function register_have_default_db_user_requirement()
 				$modules_with_database_requirements = array_filter($shared_module_info, function($item){
 					return is_array($item) && isset($item["database"]);
 				});
+
+				$failed_user_grants = [];
+				$db_details = [
+					"host" => Config::dbInfo('host'),
+					"username" => $default_db_username,
+					"password" => $default_db_password
+				];
 				foreach (array_keys($modules_with_database_requirements) as $mod)
 				{
-					$db_details = [
-						"dbname" => $shared_module_info["provided"]["get_db_connection"]($dbname),
-						"host" => Config::dbInfo('host'),
-						"username" => $default_db_username,
-						"password" => $default_db_password
-					];
-					$dbname = $shared_module_info[$mod]["db_name"];
+					$db_details["dbname"] = $shared_module_info[$mod]["db_name"];
 					try
 					{
+						$db = $shared_module_info["provided"]["get_db_connection"]( $db_details["dbname"] );
 						$db->processQuery("GRANT SELECT, INSERT, UPDATE, DELETE ON {$db_details["dbname"]}.* TO {$db_details["username"]}@{$db_details["host"]} IDENTIFIED BY '{$db_details["password"]}'");
 					}
 					catch (Exception $e)
 					{
-						$PARENT_MODULE_VARS = $MODULE_VARS;
-						$shared_module_info["registerPostInstallationTest"]([
-							"uid" => "check_user_has_access_to_{$db_details["dbname"]}",
-							"translatable_title" => sprintf(_("Check %s Has Access"), $default_db_username),
-							"installer" => function($shared_module_info) use ($db_details) {
-								$return = new stdClass();
-								$return->yield = new stdClass();
-								$return->success = true;
-								$return->yield->title = _("Check DB User Has Access");
-
-								$db_conn = @new mysqli($db_details["host"], $db_details["username"], $db_details["password"], $db_details["dbname"]);
-								if ($db_conn->connect_errno)
-								{
-									$return->yield->messages[] = sprintf(_('<b>DB Access:</b> User "%s" does not have access to database "%s" (you will need to manually grant permissions). Try:'), $db_details["username"], $db_details["dbname"])
-									 . "<br />GRANT SELECT, INSERT, UPDATE, DELETE ON {$db_details["dbname"]}.* TO {$db_details["username"]}@{$db_details["host"]} IDENTIFIED BY '{$db_details["password"]}'";
-									$return->success = false;
-								}
-								return $return;
-							}
-						]);
+						$failed_user_grants[] = $db_details["dbname"];
 					}
+				}
+				if (!empty($failed_user_grants))
+				{
+					$PARENT_MODULE_VARS = $MODULE_VARS;
+					$shared_module_info["registerPostInstallationTest"]([
+						"uid" => "check_user_has_access_to_{$db_details["dbname"]}",
+						"translatable_title" => sprintf(_("Check %s Has Access"), $default_db_username),
+						"installer" => function($shared_module_info) use ($db_details, $failed_user_grants) {
+							$return = new stdClass();
+							$return->yield = new stdClass();
+							$return->success = false;
+							$return->yield->messages = [];
+							$return->yield->title = _("Check DB User Has Access To Databases");
+
+							$db_conn = @new mysqli($db_details["host"], $db_details["username"], $db_details["password"]);
+							$result = $db->query("SHOW GRANTS FOR CURRENT_USER;");
+							$grants = $result->fetch_all(MYSQLI_ASSOC);
+							$grants = array_map('array_shift', $grants);
+							foreach ($failed_user_grants as $dbname)
+							{
+								$grant = array_filter($grants, function($var) use ($dbname) {
+									return preg_match("/\b$dbname\b/i", $var);
+								});
+								//remove everything but permissions
+								$privileges = preg_replace('/^GRANT(.*)ON.*$/i', '$1', $grant);
+								$priv_array = array_map('trim', explode(',', $str));
+								$return->yield->messages[] = $dbname . " => " . var_export($priv_array, true);
+								//TODO: DOESN'T LOOK AS THOUGH WE'RE GETTING INTO THIS INSTALLATION TEST (CHECK INSTALLER.PHP'S WHILE LOOP)
+							}
+
+							if ($db_conn->connect_errno)
+							{
+								$return->yield->messages[] = sprintf(_('<b>DB Access:</b> User "%s" does not have access to database "%s" (you will need to manually grant permissions). Try:'), $db_details["username"], $db_details["dbname"])
+								 . "<br />GRANT SELECT, INSERT, UPDATE, DELETE ON {$db_details["dbname"]}.* TO {$db_details["username"]}@{$db_details["host"]} IDENTIFIED BY '{$db_details["password"]}'";
+								$return->success = false;
+							}
+							return $return;
+						}
+					]);
 				}
 				$shared_module_info["setSharedModuleInfo"]($MODULE_VARS["uid"], "username", $default_db_username);
 				$shared_module_info["setSharedModuleInfo"]($MODULE_VARS["uid"], "password", $default_db_password);
