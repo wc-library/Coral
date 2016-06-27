3b9af63914365fc83bc085f28f8a4f3ca62607ad
James Cuénod <j3frea@gmail.com>
2016-06-27 15:50:41 -0500

    Moving session_start

--- install.php
+++ install.php
@@ -22,6 +22,7 @@
 
 // TODO: go through template.php and remove hard coded vars
 
+session_start();
 // INSTALLATION_VERSION should be a version number that version_compare will understand
 const INSTALLATION_VERSION = "0.1.0";
 const INSTALLATION_IN_PROGRESS = true;
@@ -41,59 +42,65 @@ if (!isset($_POST["installing"]))
 	exit();
 }
 
-
-require "install/test_results_yielder.php";
-require_once "common/Config.php";
-$common_config = parse_ini_file(Config::CONFIG_FILE_PATH, true);
-$old_version = isset($common_config["installation_details"]["version"]) ? $common_config["installation_details"]["version"] : false;
 $root_installation_namespace = "installation_root";
-if (!$old_version)
+require "install/test_results_yielder.php";
+if (!isset($_SESSION[$root_installation_namespace . "_do_install_anyway"]) || (isset($_SESSION[$root_installation_namespace . "_do_install_anyway"]) && $_SESSION[$root_installation_namespace . "_do_install_anyway"] !== true))
 {
-	// Installed not with unified installer
-	// OR not installed
-}
-elseif ((isset($_POST[$root_installation_namespace . "_options_button"]) && $_POST[$root_installation_namespace . "_options_button"] !== "install_anyway") || !isset($_POST[$root_installation_namespace . "_options_button"]))
-{
-	$option_button_set = [
-		[ "name" => "go_to_root", "title" => _("Take Me Home"), "custom_javascript" => 'window.location.href="index.php";' ],
-		//TODO: fix upgrade path
-		[ "name" => "try_upgrade", "title" => _("Try To Upgrade"), "custom_javascript" => 'window.location.href="upgrade.php";' ],
-		[ "name" => "install_anyway", "title" => _("Install Anyway") ],
-	];
-	$allowed_options = function($allowed_array) use ($option_button_set) {
-		return array_filter($option_button_set, function($item) use ($allowed_array) {
-			return in_array($item["name"], $allowed_array);
-		});
-	};
-
-	$yield = new stdClass();
-	$yield->messages = [];
-	$yield->title = _("CORAL Is Already Installed");
-
-	$instruction = "";
-	$option_buttons = [];
-
-	if (version_compare(INSTALLATION_VERSION, $old_version) > 0)
+	require_once "common/Config.php";
+	$common_config = parse_ini_file(Config::CONFIG_FILE_PATH, true);
+	$old_version = isset($common_config["installation_details"]["version"]) ? $common_config["installation_details"]["version"] : false;
+	if (!$old_version)
 	{
-		// This installer installs a newer version
-		$instruction = _("This installer installs a newer versino of CORAL than the one currently installed. This is <b>highly discouraged</b> and will probably result in the loss of data. Instead you should try to upgrade.");
-		$option_buttons = $allowed_options(["go_to_root", "try_upgrade", "install_anyway"]);
+		// Installed not with unified installer
+		// OR not installed
 	}
-	else if (version_compare(INSTALLATION_VERSION, $old_version) === 0)
+	elseif ((isset($_POST[$root_installation_namespace . "_option_button"]) && $_POST[$root_installation_namespace . "_option_button"] !== "install_anyway") || !isset($_POST[$root_installation_namespace . "_option_button"]))
 	{
-		// Already installed and current version
-		$instruction = _("You already have the current version installed. Are you looking for the home page?");
-		$option_buttons = $allowed_options(["go_to_root"]);
+		$option_button_set = [
+			[ "name" => "go_to_root", "title" => _("Take Me Home"), "custom_javascript" => 'window.location.href="index.php";' ],
+			//TODO: fix upgrade path
+			[ "name" => "try_upgrade", "title" => _("Try To Upgrade"), "custom_javascript" => 'window.location.href="upgrade.php";' ],
+			[ "name" => "install_anyway", "title" => _("Install Anyway") ],
+		];
+		$allowed_options = function($allowed_array) use ($option_button_set) {
+			return array_filter($option_button_set, function($item) use ($allowed_array) {
+				return in_array($item["name"], $allowed_array);
+			});
+		};
+
+		$yield = new stdClass();
+		$yield->messages = [];
+		$yield->title = _("CORAL Is Already Installed");
+
+		$instruction = "";
+		$option_buttons = [];
+
+		if (version_compare(INSTALLATION_VERSION, $old_version) > 0)
+		{
+			// This installer installs a newer version
+			$instruction = _("This installer installs a newer version of CORAL than the one currently installed. This is <b>highly discouraged</b> and will probably result in the loss of data. Instead you should try to upgrade.");
+			$option_buttons = $allowed_options(["go_to_root", "try_upgrade", "install_anyway"]);
+		}
+		else if (version_compare(INSTALLATION_VERSION, $old_version) === 0)
+		{
+			// Already installed and current version
+			$instruction = _("You already have the current version installed. Are you looking for the home page?");
+			$option_buttons = $allowed_options(["go_to_root"]);
+		}
+		else if (version_compare(INSTALLATION_VERSION, $old_version) < 0)
+		{
+			// Apparently the already installed version is newer than this installer
+			$instruction = _("The installed version of CORAL is newer than the version this installer would try to install. Are you looking for the home page or perhaps trying to upgrade?");
+			$option_buttons = $allowed_options(["go_to_root", "try_upgrade"]);
+		}
+		require_once "install/templates/option_buttons_template.php";
+		$yield->body = option_buttons_template($instruction, $option_buttons, $root_installation_namespace);
+		yield_test_results_and_exit($yield, [], 0);
 	}
-	else if (version_compare(INSTALLATION_VERSION, $old_version) < 0)
+	elseif (isset($_POST[$root_installation_namespace . "_option_button"]) && $_POST[$root_installation_namespace . "_option_button"] == "install_anyway")
 	{
-		// Apparently the already installed version is newer than this installer
-		$instruction = _("The installed version of CORAL is newer than the version this installer would try to install. Are you looking for the home page or perhaps trying to upgrade?");
-		$option_buttons = $allowed_options(["go_to_root", "try_upgrade"]);
+		$_SESSION[$root_installation_namespace . "_do_install_anyway"] = true;
 	}
-	require_once "install/templates/option_buttons_template.php";
-	$yield->body = option_buttons_template($instruction, $option_buttons, $root_installation_namespace);
-	yield_test_results_and_exit($yield, [], 0);
 }
 
 require "install/installer.php";
--- install/installer.php
+++ install/installer.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 require_once("common/DBService.php");
 require_once("common/DBResult.php");
 
