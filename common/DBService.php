wc-library/Coral

commit ed5072e80bf369fa9895e4f87b95eaff49d15ab1

DBService modified for previous changes to Config.php

common/DBService.php
@@ -35,7 +35,14 @@ class DBService extends Object {
 			 *        (we do this so that mysqli doesn't spit out warnings if
 			 *        the connection fails) we must manually handle errors!
 			 */
-			self::$db = @new mysqli(Config::dbInfo("host"), Config::dbInfo("username"), Config::dbInfo("password"));
+			// Load this data outside of the mysqli call so that if an error occurs it is thrown.
+			$dbInfo = [
+				"host" => Config::dbInfo("host"),
+				"username" => Config::dbInfo("username"),
+				"password" => Config::dbInfo("password")
+			];
+
+			self::$db = new mysqli($dbInfo["host"], $dbInfo["username"], $dbInfo["password"]);
 			if (self::$db->connect_errno)
 			{
 				switch (self::$db->connect_errno) {

commit 0804d8fe36a1875aee25fb19103d34ec1dc07441

Fixing regression from ed5072e that stops suppressing mysqli vomit

common/DBService.php
@@ -42,7 +42,7 @@ class DBService extends Object {
 				"password" => Config::dbInfo("password")
 			];
 
-			self::$db = new mysqli($dbInfo["host"], $dbInfo["username"], $dbInfo["password"]);
+			self::$db = @new mysqli($dbInfo["host"], $dbInfo["username"], $dbInfo["password"]);
 			if (self::$db->connect_errno)
 			{
 				switch (self::$db->connect_errno) {