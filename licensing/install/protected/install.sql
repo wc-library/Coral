d8e90721538f9aafc047f7c433458d7640e12324
Victor Grousset/tuxayo <victor@tuxayo.net>
2016-09-05 10:09:39 +0200

    Fix default timestamps to be compatible with MySQL 5.7 #50

--- licensing/install/protected/install.sql
+++ licensing/install/protected/install.sql
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `Expression` (
   `expressionTypeID` int(10) unsigned NOT NULL,
   `documentText` text,
   `simplifiedText` text,
-  `lastUpdateDate` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `lastUpdateDate` timestamp NOT NULL default CURRENT_TIMESTAMP,
   `productionUseInd` tinyint(1) NOT NULL default '0',
   PRIMARY KEY  (`expressionID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `ExpressionNote` (
   `expressionID` int(10) default NULL,
   `note` varchar(2000) default NULL,
   `displayOrderSeqNumber` int(10) default NULL,
-  `lastUpdateDate` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `lastUpdateDate` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`expressionNoteID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
