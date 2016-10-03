5332f30523ba11df0551591a5b650b70b13b5fcb
Connor de la Cruz <connor.c.delacruz@gmail.com>
2016-10-03 14:39:34 -0500

    Completed Licensing acceptance test and wrote an additional acceptance test for Organizations

--- tests/acceptance/LicensingCept.php
+++ tests/acceptance/LicensingCept.php
@@ -6,14 +6,14 @@ $I->wantTo('ensure that I can create/delete a license and see it in the list');
 $I->amOnPage("/licensing/");
 $I->click("New License");
 $I->fillField("#licenseShortName", "Test License");
-$I->fillField("#organizationName", "Test Organization");
+$I->fillField("#organizationName", "Test Publisher");
 $I->click(".submit-button");
 $I->waitForText("License Added Successfully.", 5);
 $I->click("Continue");
 // we are redirected to the test license's page
 $I->waitForText("Edit License", 5); // ensure that we are on the test license's page
 
-// Find license in list an go to it's page
+// Find license in list and go to its page
 $I->amOnPage("/licensing/");
 $I->waitForPageToBeReady(); // Ensure that the list has loaded by Ajax
 $I->click("Test License");
