3d6763aa2c81e1e01010b0c30d420bf4f0c87c26
Tommy Keswick <tkeswick@caltech.edu>
2017-01-04 17:00:23 -0800

    Issue #150: Adjust timestamp format for MySQL.

--- usage/admin/classes/domain/SushiService.php
+++ usage/admin/classes/domain/SushiService.php
@@ -300,7 +300,7 @@ class SushiService extends DatabaseObject {
 		//save to import log!!
 		$importLog = new ImportLog();
 		$importLog->loginID = "sushi";
-		$ImportLog->importDateTime = time();
+		$importLog->importDateTime = date('Y-m-d H:i:s');
 		$importLog->layoutCode = $reportLayout;
 		$importLog->fileName = 'archive/' . $txtFile;
 		$importLog->archiveFileURL = 'archive/' . $txtFile;
