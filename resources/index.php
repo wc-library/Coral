2819a91cb293f7217d84c4e5c3de73dde4dc947b
jeffnm <jeffmudge@gmail.com>
2017-02-27 14:47:00 -0600

    Additional Search Fields
    Adding search fields for Publisher, Platform, and Provider.

--- resources/index.php
+++ resources/index.php
@@ -71,6 +71,34 @@ include 'templates/header.php';
 	</tr>
 
 
+	<tr>
+	<td class='searchRow'><label for='searchPublisher'><b>Publisher (contains)</b></label>
+	<br />
+	<?php echo Html::text_search_field_tag('publisher', $search['publisher']); ?>
+	<br />
+	<div id='div_searchPublisher' style='<?php if (!$search['publisher']) echo "display:none;"; ?>margin-left:123px;'><input type='button' name='btn_searchPublisher' value='<?php echo _("go!");?>' class='searchButton' /></div>
+	</td>
+	</tr>
+
+	<tr>
+	<td class='searchRow'><label for='searchPlatform'><b>Platform (contains)</b></label>
+	<br />
+	<?php echo Html::text_search_field_tag('platform', $search['platform']); ?>
+	<br />
+	<div id='div_searchPlatform' style='<?php if (!$search['platform']) echo "display:none;"; ?>margin-left:123px;'><input type='button' name='btn_searchPlatform' value='<?php echo _("go!");?>' class='searchButton' /></div>
+	</td>
+	</tr>
+
+	<tr>
+	<td class='searchRow'><label for='searchProvider'><b>Provider (contains)</b></label>
+	<br />
+	<?php echo Html::text_search_field_tag('provider', $search['provider']); ?>
+	<br />
+	<div id='div_searchProvider' style='<?php if (!$search['provider']) echo "display:none;"; ?>margin-left:123px;'><input type='button' name='btn_searchProvider' value='<?php echo _("go!");?>' class='searchButton' /></div>
+	</td>
+	</tr>
+
+
 
 	<tr>
 	<td class='searchRow'><label for='searchResourceISBNOrISSN'><b><?php echo _("ISBN/ISSN");?></b></label>
