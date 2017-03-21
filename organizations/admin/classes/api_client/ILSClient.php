59d6ce409318c94803ad5771acb2fff555a5eb82
Matthias Meusburger <matthias.meusburger@biblibre.com>
2017-03-21 09:04:57 +0000

    Updates interface Adds class comments

--- organizations/admin/classes/api_client/ILSClient.php
+++ organizations/admin/classes/api_client/ILSClient.php
@@ -3,6 +3,9 @@
 interface ILSClient {
     function addVendor($vendor);
     function getVendor($id);
+    function getVendorByName($name);
+    function getVendorByExactName($name);
+    function vendorExits($name);
     function getILSName();
     function getILSURL();
 }
--- organizations/admin/classes/api_client/KohaClient.php
+++ organizations/admin/classes/api_client/KohaClient.php
@@ -3,6 +3,9 @@
 require '../resources/api_client/vendor/autoload.php';
 require 'ILSClient.php';
 
+/**
+ * KohaClient
+ */
 class KohaClient implements ILSClient {
 
     private $server;
@@ -16,6 +19,11 @@ class KohaClient implements ILSClient {
         $this->kohaToCoralKeys = array_flip($this->coralToKohaKeys);
     }
 
+    /**
+     * Adds a vendor in the ILS
+     * @param $vendor key-value array with vendor description
+     * @return id of the vendor in the ils
+     */ 
     function addVendor($vendor) {
         $headers = array("Accept" => "application/json");
         $body = Unirest\Request\Body::json($this->_vendorToKoha($vendor));
@@ -24,45 +32,84 @@ class KohaClient implements ILSClient {
         //return ($response->body->id) ? $response->body->id : $response->raw_body;
     }
 
+    /**
+     * Gets a vendor from the ILS
+     * @param id of the vendor in the ils
+     * @return key-value array with vendor description
+     */
     function getVendor($id) {
         $response = Unirest\Request::get($this->server . "/acquisitions/vendors/$id");
         return $this->_vendorToCoral((array) $response->body);
     }
 
+     /**
+     * Gets a vendor from the ILS
+     * @param name of the vendor in the ils
+     * @return key-value array with vendor description
+     */
     function getVendorByName($name) {
         $response = Unirest\Request::get($this->server . "/acquisitions/vendors/?name=$name");
         return $this->_vendorToCoral((array) $response->body);
     }
 
+    /**
+     * Gets a vendor from the ILS
+     * @param name of the vendor in the ils
+     * @return key-value array with vendor description
+     */
     function getVendorByExactName($name) {
         $response = Unirest\Request::get($this->server . "/acquisitions/vendors/?exactname=$name");
         return $this->_vendorToCoral((array) $response->body);
     }
 
+    /**
+     * Does a vendor exist in the ILS?
+     * @param name of the vendor in the ils
+     * @return boolean
+     */
     function vendorExists($name) {
         $response = Unirest\Request::get($this->server . "/acquisitions/vendors/?exactname=$name");
         return (count((array) $response->body) > 0) ? true : false;;
     }
 
+    /**
+     * Gets the ILS name
+     * @return the ILS name
+     */
     function getILSName() {
         return "Koha";
     }
     
-
+    /**
+     * Gets the ILS API url
+     * @return the ILS API url
+     */
     function getILSURL() {
         return $this->server;
     }
 
+    /**
+     * Changes the keys of a vendor array from Koha keys to Coral keys
+     */
     private function _vendorToCoral($vendor) {
         $kohaToCoralKeys = $this->kohaToCoralKeys;
         return $this->_changeKeys($vendor, $kohaToCoralKeys);
     }
 
+    /**
+     * Changes the keys of a vendor array from Coral keys to Koha keys
+     */
     private function _vendorToKoha($vendor) {
         $coralToKohaKeys = $this->coralToKohaKeys;
         return $this->_changeKeys($vendor, $coralToKohaKeys);
     }
 
+    /**
+     * Changes the keys of an array
+     * @param $array a key/value array
+     * @param $keys an array containing $oldKey => $newKey key/values
+     * @return the modified array with the new keys
+     */
     private function _changeKeys($array, $keys) {
         foreach ($keys as $oldKey => $newKey) {
             if (array_key_exists($oldKey, $array)) {
