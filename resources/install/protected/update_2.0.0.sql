5839589fb0d107f9d4bec36a9c2be4e152d68c5e
Matthias Meusburger <matthias.meusburger@biblibre.com>
2017-03-28 08:20:32 +0000

    Issue 190: Error on routing screen - ResourceStep missing dateArchive

--- resources/install/protected/update_2.0.0.sql
+++ resources/install/protected/update_2.0.0.sql
@@ -48,3 +48,8 @@ ALTER TABLE `ResourcePayment`
 ALTER TABLE `ResourcePayment` DROP COLUMN `fundName`;
 
 ALTER TABLE  `ResourceNote` MODIFY `updateDate` timestamp NOT NULL default CURRENT_TIMESTAMP;
+
+ALTER TABLE  `ResourceStep` ADD  `archivingDate` DATETIME NULL AFTER  `stepEndDate` ;
+INSERT INTO `ResourceType` (`resourceTypeID`, `shortName`, `includeStats`) VALUES (NULL, 'Any', NULL);
+INSERT INTO `ResourceFormat` (`resourceFormatID`, `shortName`) VALUES (NULL, 'Any');
+INSERT INTO `AcquisitionType` (`acquisitionTypeID`, `shortName`) VALUES (NULL, 'Any');
