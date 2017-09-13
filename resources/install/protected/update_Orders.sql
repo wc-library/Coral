be12a1e4890ced8ae3946888467263c199fb8914
Matthias Meusburger <matthias.meusburger@biblibre.com>
2017-09-13 16:05:06 +0200

    Use single SQL file for the update

--- resources/install/protected/update_Orders.sql
+++ resources/install/protected/update_Orders.sql
@@ -1,3 +1,4 @@
+-- Update schema
 CREATE TABLE `ResourceAcquisition` (
   `resourceAcquisitionID` int(11) NOT NULL,
   `resourceID` int(11) NOT NULL,
@@ -55,11 +56,96 @@ ALTER TABLE `Downtime` ADD `resourceAcquisitionID` INT(11) NULL DEFAULT NULL AFT
 ALTER TABLE `ResourceStep` CHANGE `resourceID` `resourceAcquisitionID` INT(11) NULL DEFAULT NULL;
 ALTER TABLE `ResourceNote` CHANGE `resourceID` `entityID` INT(11) NULL DEFAULT NULL;
 
+-- Migrate existing data
+INSERT INTO ResourceAcquisition (resourceID,
+orderNumber,
+systemNumber,
+acquisitionTypeID,
+subscriptionAlertEnabledInd,
+authenticationTypeID,
+authenticationUserName,
+authenticationPassword,
+accessMethodID,
+storageLocationID,
+userLimitID,
+coverageText,
+bibSourceURL,
+catalogingTypeID,
+catalogingStatusID,
+numberRecordsAvailable,
+numberRecordsLoaded,
+recordSetIdentifier,
+hasOclcHoldings,
+workflowRestartDate,
+workflowRestartLoginID,subscriptionStartDate, subscriptionEndDate) SELECT Resource.resourceID,
+Resource.orderNumber,
+Resource.systemNumber,
+Resource.acquisitionTypeID,
+Resource.subscriptionAlertEnabledInd,
+Resource.authenticationTypeID,
+Resource.authenticationUserName,
+Resource.authenticationPassword,
+Resource.accessMethodID,
+Resource.storageLocationID,
+Resource.userLimitID,
+Resource.coverageText,
+Resource.bibSourceURL,
+Resource.catalogingTypeID,
+Resource.catalogingStatusID,
+Resource.numberRecordsAvailable,
+Resource.numberRecordsLoaded,
+Resource.recordSetIdentifier,
+Resource.hasOclcHoldings,
+Resource.workflowRestartDate,
+Resource.workflowRestartLoginID,NOW(),NOW() FROM Resource;
 
+UPDATE ResourcePurchaseSiteLink LEFT JOIN ResourceAcquisition ON ResourcePurchaseSiteLink.resourceAcquisitionID = ResourceAcquisition.resourceID SET ResourcePurchaseSiteLink.resourceAcquisitionID = ResourceAcquisition.resourceAcquisitionID;
 
+UPDATE ResourcePayment LEFT JOIN ResourceAcquisition ON ResourcePayment.resourceAcquisitionID = ResourceAcquisition.resourceID SET ResourcePayment.resourceAcquisitionID = ResourceAcquisition.resourceAcquisitionID;
 
+UPDATE ResourceAdministeringSiteLink LEFT JOIN ResourceAcquisition ON ResourceAdministeringSiteLink.resourceAcquisitionID = ResourceAcquisition.resourceID SET ResourceAdministeringSiteLink.resourceAcquisitionID = ResourceAcquisition.resourceAcquisitionID;
 
--- IMPORT FROM OLD FIELDS
+UPDATE ResourceAuthorizedSiteLink LEFT JOIN ResourceAcquisition ON ResourceAuthorizedSiteLink.resourceAcquisitionID = ResourceAcquisition.resourceID SET ResourceAuthorizedSiteLink.resourceAcquisitionID = ResourceAcquisition.resourceAcquisitionID;
 
+UPDATE Attachment LEFT JOIN ResourceAcquisition ON Attachment.resourceAcquisitionID = ResourceAcquisition.resourceID SET Attachment.resourceAcquisitionID = ResourceAcquisition.resourceAcquisitionID;
 
--- REMOVE OLD FIELDS
+UPDATE Contact LEFT JOIN ResourceAcquisition ON Contact.resourceAcquisitionID = ResourceAcquisition.resourceID SET Contact.resourceAcquisitionID = ResourceAcquisition.resourceAcquisitionID;
+
+UPDATE ResourceLicenseLink LEFT JOIN ResourceAcquisition ON ResourceLicenseLink.resourceAcquisitionID = ResourceAcquisition.resourceID SET ResourceLicenseLink.resourceAcquisitionID = ResourceAcquisition.resourceAcquisitionID;
+
+UPDATE ResourceLicenseStatus LEFT JOIN ResourceAcquisition ON ResourceLicenseStatus.resourceAcquisitionID = ResourceAcquisition.resourceID SET ResourceLicenseStatus.resourceAcquisitionID = ResourceAcquisition.resourceAcquisitionID;
+
+UPDATE ResourceStep LEFT JOIN ResourceAcquisition ON ResourceStep.resourceAcquisitionID = ResourceAcquisition.resourceID SET ResourceStep.resourceAcquisitionID = ResourceAcquisition.resourceAcquisitionID;
+
+UPDATE Downtime LEFT JOIN ResourceAcquisition ON Downtime.entityID = ResourceAcquisition.resourceID SET Downtime.resourceAcquisitionID = ResourceAcquisition.resourceAcquisitionID;
+
+UPDATE IssueRelationship LEFT JOIN ResourceAcquisition ON IssueRelationship.entityID = ResourceAcquisition.resourceID SET IssueRelationship.resourceAcquisitionID = ResourceAcquisition.resourceAcquisitionID;
+
+UPDATE ResourceNote LEFT JOIN ResourceAcquisition ON ResourceNote.entityID = ResourceAcquisition.resourceID SET ResourceNote.entityID = ResourceAcquisition.resourceAcquisitionID WHERE UPPER(tabName) = 'CATALOGING';
+
+UPDATE ResourceNote LEFT JOIN ResourceAcquisition ON ResourceNote.entityID = ResourceAcquisition.resourceID SET ResourceNote.entityID = ResourceAcquisition.resourceAcquisitionID WHERE UPPER(tabName) = 'ACCESS';
+UPDATE ResourceNote LEFT JOIN ResourceAcquisition ON ResourceNote.entityID = ResourceAcquisition.resourceID SET ResourceNote.entityID = ResourceAcquisition.resourceAcquisitionID WHERE UPPER(tabName) = 'ACQUISITIONS';
+
+-- Drop old fields
+ALTER TABLE `Resource`
+  DROP `workflowRestartDate`,
+  DROP `workflowRestartLoginID`,
+  DROP `orderNumber`,
+  DROP `systemNumber`,
+  DROP `subscriptionAlertEnabledInd`,
+  DROP `userLimitID`,
+  DROP `authenticationUserName`,
+  DROP `authenticationPassword`,
+  DROP `storageLocationID`,
+  DROP `acquisitionTypeID`,
+  DROP `authenticationTypeID`,
+  DROP `accessMethodID`,
+  DROP `recordSetIdentifier`,
+  DROP `hasOclcHoldings`,
+  DROP `numberRecordsAvailable`,
+  DROP `numberRecordsLoaded`,
+  DROP `bibSourceURL`,
+  DROP `catalogingTypeID`,
+  DROP `catalogingStatusID`,
+  DROP `coverageText`;
+
