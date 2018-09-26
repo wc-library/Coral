0b9cab80f12cb7326718f30e7c80aee8a2fd94a1
Luke Aeschleman <lukeaeschleman@gmail.com>
2018-09-26 17:51:04 -0400

    throw exception if terms tool service not configured

--- licensing/admin/classes/domain/TermsService.php
+++ licensing/admin/classes/domain/TermsService.php
@@ -37,6 +37,9 @@ class TermsService extends Object {
 	public function getTermsToolObj() {
 
 		//return correct object for terms tool
+        if (empty($$this->config->terms->resolver)) {
+            throw new Exception("Terms tool service is not configured. Please setup a terms tool service in the Licensing > Admin module");
+        }
 		$className = $this->config->terms->resolver . "Service";
 
 		try{
