5d2216a214cdceb495c955e6fb520276a279e7b0
jeffnm <jeffmudge@gmail.com>
2018-10-26 08:13:30 -0500

    SQL to copy Resource table Start/End Dates to ResourceAcquisition table when they exist and the ResourceAcquisition table doesn't look like it has an updated date. Drop the Resource table currentStartDate and currentEndDate columns afterwards.

---
+++ resources/install/protected/3.0.1/002-489.sql
@@ -0,0 +1,22 @@
+# Get Resource, check if currentStartDate and currentEndDate are NULL or 0000-00-00
+# If not, then check ResourceAcquisition subscriptionStartDate and subscriptionEndDate;
+# if they are identical to each other, replace them with the data from Resource
+# If ResourceAcquisition subscriptionStartDate and subscriptionEndDate are different from each other,
+# assume they've intentionally been updated and leave them alone.
+
+UPDATE ResourceAcquisition RA
+JOIN Resource R ON R.resourceID = RA.resourceID
+SET
+RA.subscriptionStartDate=R.currentStartDate,
+RA.subscriptionEndDate=R.currentEndDate
+WHERE
+((R.currentStartDate IS NOT NULL) AND (R.currentStartDate <> "0000-00-00")
+  OR (R.currentEndDate IS NOT NULL) AND (R.currentEndDate <> "0000-00-00"))
+AND R.currentStartDate != R.currentEndDate
+AND RA.subscriptionStartDate = RA.subscriptionEndDate;
+
+# Drop the Resource currentStartDate and currentEndDate columns
+
+ALTER TABLE Resource
+  DROP COLUMN currentStartDate,
+  DROP COLUMN currentEndDate;
