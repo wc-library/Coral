d1845e767198a21b144a3e52c92476ca571a662f
Luke Aeschleman <lukeaeschleman@gmail.com>
2019-02-19 08:09:37 -0500

    wip

--- resources/templates/ebscoKbPackageList.php
+++ resources/templates/ebscoKbPackageList.php
@@ -2,11 +2,10 @@
     <thead>
         <tr>
             <th><?php echo _("Name"); ?></th>
-            <th><?php echo _("Imported?"); ?></th>
             <th><?php echo _("Title Count"); ?></th>
             <th><?php echo _("Content Type"); ?></th>
-            <th></th>
-            <th></th>
+            <th><?php echo _("Imported?"); ?></th>
+            <th><?php echo _("Current Holdings"); ?></th>
         </tr>
     </thead>
     <tbody>
@@ -16,14 +15,10 @@
             <td>
                 <?php echo $item->packageName; ?>
                 <br>
-                <small>(<?php echo $item->vendorName; ?>)</small>
-            </td>
-            <td style="text-align: center;">
-                <?php if($item->resource): ?>
-                    <a href="resource.php?resourceID=<?php echo $item->resource->primaryKey; ?>">
-                        <i class="fa fa-check text-success" title="<?php echo _('imported in Coral'); ?>"></i>
-                    </a>
-                <?php endif; ?>
+                <small>(<?php echo $item->vendorName; ?>)</small> <a class="setPackage"
+                                                                     data-vendor-id="<?php echo $item->vendorId; ?>"
+                                                                     data-package-id="<?php echo $item->packageId; ?>"
+                                                                     data-package-name="<?php echo $item->packageName; ?>"><?php echo _("view titles"); ?></a>
             </td>
             <td>
                 <?php echo $item->titleCount; ?>
@@ -36,21 +31,29 @@
                 <?php echo $item->contentType; ?>
             </td>
             <td style="text-align: center;">
-                <button
-                        class="setPackage add-button"
-                        data-vendor-id="<?php echo $item->vendorId; ?>"
-                        data-package-id="<?php echo $item->packageId; ?>"
-                        data-package-name="<?php echo $item->packageName; ?>"
-                >
-                    <?php echo _("View Titles"); ?>
-                </button>
+                <?php if($item->resource): ?>
+                    <a href="resource.php?resourceID=<?php echo $item->resource->primaryKey; ?>">
+                        <i class="fa fa-check text-success" title="<?php echo _('imported in Coral'); ?>"></i>
+                    </a>
+                <?php elseif ($item->selectedCount): ?>
+                    <i class="fa fa-ban text-danger" title="Imported but not selected in EBSCO"></i>
+                    <a href="ajax_forms.php?action=getEbscoKbPackageImportForm&height=700&width=730&modal=true&vendorId=<?php echo $item->vendorId; ?>&packageId=<?php echo $item->packageId; ?>"
+                       class="thickbox">
+                        <?php echo _('Import Package'); ?>
+                    </a>
+                <?php endif; ?>
             </td>
             <td style="text-align: center;">
-                <a
-                    href="ajax_forms.php?action=getEbscoKbPackageImportForm&height=700&width=730&modal=true&vendorId=<?php echo $item->vendorId; ?>&packageId=<?php echo $item->packageId; ?>"
-                    class="thickbox btn btn-primary">
-                    <?php echo _('import'); ?>
-                </a>
+                <?php if($item->selectedCount): ?>
+                    <a href="#" class="btn btn-success">
+                        <i class="fa fa-check"></i> <?php echo _('Selected'); ?> <i class="fa fa-chevron-down"></i>
+                    </a>
+                <?php else: ?>
+                    <a href="#" class="btn">
+                        <?php echo _('Not Selected'); ?> <i class="fa fa-chevron-down"></i>
+                    </a>
+                <?php endif; ?>
+
             </td>
         </tr>
     <?php endforeach; ?>
