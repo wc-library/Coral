24a739e55052781ceaf2cec8ad92b2ef3755aec7
Mathias Tranzer <mathiastranzer@gmail.com>
2019-03-29 11:16:24 +0100

    Change 'import fund' black icon to color

--- resources/ajax_htmldata/getAdminFundDisplay.php
+++ resources/ajax_htmldata/getAdminFundDisplay.php
@@ -8,7 +8,7 @@
 		<div class='adminHeader'>
 			<div><?php echo "<div class='adminRightHeader'>" . _("Fund") . "</div>"; ?></div>
 			<div class='addElement'><?php echo "<a href='ajax_forms.php?action=getAdminFundUpdateForm&updateID=&height=178&width=260&modal=true' class='thickbox'><img id='addFund' src='images/plus.gif' title='"._("add fund")."'/></a><br/>";?></div>
-			<div class='ImportElement'><?php echo "<a href='importFunds.php?action=getAdminFundUpdateForm&updateID=&height=175&width=300&modal=true' class='thickbox'><img id='ImportFund'src='images/import-fund.gif' title='"._("import fund")."'/></a>"; ?></div>
+			<div class='ImportElement'><?php echo "<a href='importFunds.php?action=getAdminFundUpdateForm&updateID=&height=175&width=300&modal=true' class='thickbox'><img id='ImportFund'src='images/fund-import-blueGrey_12px.png' title='"._("import fund")."'/></a>"; ?></div>
 
 		</div>
 
