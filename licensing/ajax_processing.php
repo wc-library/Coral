0a34ae679b6d15e88b59bb637e5580e38a636ff0
Matthias Meusburger <matthias.meusburger@biblibre.com>
2019-11-27 17:12:55 +0100

    Issue 458: Coral silently fails on too large file uploads
     - Follow-up: licensing

--- licensing/ajax_processing.php
+++ licensing/ajax_processing.php
@@ -950,15 +950,20 @@ switch ($_GET['action']) {
 		//if match was not found
 		//note, echoes are not being sent anywhere
 		if ($exists == 0){
-			if(move_uploaded_file($_FILES['myfile']['tmp_name'], $target_path)) {
-				//set to web rwx, everyone else rw
-				//this way we can edit the document directly on the server
-				chmod ($target_path, 0766);
-				echo _("success uploading!");
-			}else{
-				header('HTTP/1.1 500 Internal Server Error');
-			  echo "<div id=\"error\">"._("There was a problem saving your file to")." $target_path.</div>";
-			}
+            if ($_FILES['myfile']['error'] === UPLOAD_ERR_OK) {
+                if(move_uploaded_file($_FILES['myfile']['tmp_name'], $target_path)) {
+                    //set to web rwx, everyone else rw
+                    //this way we can edit the document directly on the server
+                    chmod ($target_path, 0766);
+                    echo _("success uploading!");
+                }else{
+                  header('HTTP/1.1 500 Internal Server Error');
+                  echo _("There was a problem saving your file to ") . $target_path;
+                }
+            } else {
+                header('HTTP/1.1 500 Internal Server Error');
+                echo uploadErrorMessage($_FILES['myfile']['error']);
+            }
 		}
 
 
--- licensing/templates/header.php
+++ licensing/templates/header.php
@@ -56,13 +56,22 @@ $coralURL = $util->getCORALURL();
 <link rel="SHORTCUT ICON" href="images/favicon.ico" />
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
-<script type="text/javascript" src="../js/plugins/jquery-1.4.4.js"></script>
-<script type="text/javascript" src="../js/plugins/ajaxupload.3.5.js"></script>
-<script type="text/javascript" src="js/plugins/thickbox.js"></script>
-<script type="text/javascript" src="../js/plugins/jquery.autocomplete.js"></script>
-<script type="text/javascript" src="../js/plugins/jquery.tooltip.js"></script>
 <script type="text/javascript" src="../js/plugins/Gettext.js"></script>
+<script type="text/javascript" src="../js/plugins/translate.js"></script>
+<?php if (isset($customJSInclude)) {
+    echo $customJSInclude;
+} else { ?>
+    <script type="text/javascript" src="../js/plugins/jquery-1.4.4.js"></script>
+    <script type="text/javascript" src="../js/plugins/ajaxupload.3.5.js"></script>
+    <script type="text/javascript" src="js/plugins/thickbox.js"></script>
+    <script type="text/javascript" src="../js/plugins/jquery.autocomplete.js"></script>
+    <script type="text/javascript" src="../js/plugins/jquery.tooltip.js"></script>
+    <script type="text/javascript" src="../js/plugins/datejs-patched-for-i18n.js"></script>
+    <script type="text/javascript" src="../js/plugins/jquery.datePicker-patched-for-i18n.js"></script>
+    <script type="text/javascript" src="../js/common.js"></script>
+    <script type="text/javascript" src="js/common.js"></script>
 <?php
+}
     // Add translation for the JavaScript files
     global $http_lang;
     $str = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,5);
@@ -76,11 +85,6 @@ $coralURL = $util->getCORALURL();
             echo "<link rel='gettext' type='application/x-po' href='./locale/".$http_lang."/LC_MESSAGES/messages.po' />";
     }
 ?>
-<script type="text/javascript" src="../js/plugins/translate.js"></script>
-<script type="text/javascript" src="../js/plugins/datejs-patched-for-i18n.js"></script>
-<script type="text/javascript" src="../js/plugins/jquery.datePicker-patched-for-i18n.js"></script>
-<script type="text/javascript" src="../js/common.js"></script>
-<script type="text/javascript" src="js/common.js"></script>
 <script type="text/javascript">
 Date.format = '<?php echo return_datepicker_date_format(); ?>';
 </script>
