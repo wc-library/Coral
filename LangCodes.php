a0b68b48ce2817de537fe338034b9ceaf357a26a
Matthias Meusburger <matthias.meusburger@biblibre.com>
2020-03-09 11:51:17 +0100

    Translation for 20.02:
      - Re-order languages alphabetically in dropdown list
      - Add es
      - Move es to es_ES by default

--- LangCodes.php
+++ LangCodes.php
@@ -6,25 +6,27 @@
 class LangCodes{
     public function getLanguage($code){
         $all_lang=array(
-            'fr_FR'=>'fr_FR',
+            'de_DE'=>'de_DE',
             'en_GB'=>'en_GB',
             'en_US'=>'en_US',
+            'es_ES'=> 'es_ES',
+            'fr_FR'=>'fr_FR',
+            'tr_TR'=>'tr_TR',
             'zh_CN'=>'zh_CN',
-            'zh_TW'=>'zh_TW',
-            'de_DE'=>'de_DE',
-            'tr_TR'=>'tr_TR'
+            'zh_TW'=>'zh_TW'
         );
         return array_key_exists($code, $all_lang) ? $all_lang[$code] : null;
     }
     public function getNameLang($code_lang){
         $name_lang=array(
-            'fr_FR'=>'Français',
+            'de_DE'=>'Deutsch',
             'en_GB'=>'English (GB)',
             'en_US'=>'English (US)',
+            'es_ES'=> 'Español',
+            'fr_FR'=>'Français',
+            'tr_TR'=>'Türkçe',
             'zh_CN'=>'中文 (简体)',
-            'zh_TW'=>'中文 (正體)',
-            'de_DE'=>'Deutsch',
-            'tr_TR'=>'Türkçe'
+            'zh_TW'=>'中文 (正體)'
         );
         return array_key_exists($code_lang, $name_lang) ? $name_lang[$code_lang] : null;
     }
